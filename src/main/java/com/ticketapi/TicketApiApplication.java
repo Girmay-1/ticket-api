eac2f71bbcb797f3a199fc42fcc7d9d4d4469dd0
Girmay-1 <girmaykibreab93@gmail.com>
2024-07-10 16:27:56 -0700

    updated main class and added spring boot annotation

---
+++ src/main/java/com/ticketapi/TicketApiApplication.java
@@ -0,0 +1,16 @@
+package com.ticketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class
+ *
+ */
+@SpringBootApplication
+public class TicketApiApplication {
+    public static void main( String[] args )
+    {
+        SpringApplication.run(TicketApiApplication.class, args);
+    }
+}
