7fc27b69acecefea82f92f22820eae1d66ca7458
Girmay-1 <girmaykibreab93@gmail.com>
2024-07-11 10:39:16 -0700

    added classes for contrller, model and service purposes

---
+++ src/main/java/com/ticketapi/service/UserService.java
@@ -0,0 +1,21 @@
+package com.ticketapi.service;
+
+import com.ticketapi.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public User createUser(User user) {
+        String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, user.getUsername(), user.getPassword(), user.getEmail());
+        return user;
+    }
+
+    // Implement other methods
+}
