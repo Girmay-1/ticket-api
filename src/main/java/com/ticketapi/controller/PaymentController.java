71732d29d6ff819af0a7da63c42deaa77644b32c
Girmay-1 <girmaykibreab93@gmail.com>
2024-07-24 21:41:16 -0700

    removed unused variable

--- src/main/java/com/ticketapi/controller/PaymentController.java
+++ src/main/java/com/ticketapi/controller/PaymentController.java
@@ -3,6 +3,8 @@ package com.ticketapi.controller;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.ticketapi.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,14 +18,13 @@ import java.util.Map;
 @RequestMapping("/api/payments")
 public class PaymentController {
     private final PaymentService paymentService;
-
+    private final static Logger logger = LoggerFactory.getLogger(PaymentController.class);
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     @PostMapping("/create-payment-intent")
-    public ResponseEntity<Map<String, String>> createPaymentIntent(@RequestBody Map<String, Object> paymentInfo, Authentication authentication) {
-        String username = authentication.getName();
+    public ResponseEntity<Map<String, String>> createPaymentIntent(@RequestBody Map<String, Object> paymentInfo) {
         try {
             PaymentIntent intent = paymentService.createPaymentIntent(
                     ((Number) paymentInfo.get("amount")).longValue(),
@@ -32,6 +33,7 @@ public class PaymentController {
             Map<String, String> response = Map.of("clientSecret", intent.getClientSecret());
             return ResponseEntity.ok(response);
         } catch (StripeException e) {
+            logger.error("Payment failed because the exception:", e);
             return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
         }
     }
