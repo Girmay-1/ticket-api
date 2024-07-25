e3b8e08c5b0f6ecdf2a8f1da4b65f483d7fb24b1
Girmay-1 <girmaykibreab93@gmail.com>
2024-07-24 22:08:59 -0700

    added swagger for AuthController

--- src/main/java/com/ticketapi/controller/AuthController.java
+++ src/main/java/com/ticketapi/controller/AuthController.java
@@ -4,6 +4,10 @@ import com.ticketapi.model.User;
 import com.ticketapi.util.JwtUtil;
 import com.ticketapi.service.CustomUserDetailsService;
 import com.ticketapi.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,6 +24,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
+@Api(tags = "Authentication", description = "Endpoints for user authentication")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
@@ -38,6 +43,11 @@ public class AuthController {
     }
 
     @PostMapping("/register")
+    @ApiOperation(value = "Register a new user", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully registered user"),
+            @ApiResponse(code = 400, message = "Bad request")
+    })
     public ResponseEntity<?> registerUser(@RequestBody User user) {
         try {
             User createdUser = userService.createUser(user);
@@ -49,6 +59,12 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+    @ApiOperation(value = "Authenticate a user", response = Map.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully authenticated"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public ResponseEntity<?> loginUser(@RequestBody User loginUser) {
         try {
             authenticationManager.authenticate(
