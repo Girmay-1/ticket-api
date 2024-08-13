b2a8bc6717699e8e6a340744be51f984888e6e7b
Girmay-1 <girmaykibreab93@gmail.com>
2024-08-12 17:33:09 -0700

    added the ELK stack and prometheus and grafana for metrics and monitoring

--- src/main/java/com/ticketapi/controller/AuthController.java
+++ src/main/java/com/ticketapi/controller/AuthController.java
@@ -4,8 +4,11 @@ import com.ticketapi.model.User;
 import com.ticketapi.util.JwtUtil;
 import com.ticketapi.service.CustomUserDetailsService;
 import com.ticketapi.service.UserService;
+import io.micrometer.core.annotation.Timed;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("/api/auth")
 @Tag(name = "Authentication", description = "Authentication operations")
@@ -29,6 +33,8 @@ public class AuthController {
     private final CustomUserDetailsService userDetailsService;
     private final UserService userService;
     private final JwtUtil jwtUtil;
+
+    private final static Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     public AuthController(AuthenticationManager authenticationManager,
                           CustomUserDetailsService userDetailsService,
@@ -42,11 +48,14 @@ public class AuthController {
 
     @PostMapping("/register")
     @Operation(summary = "Register a new user")
+    @Timed(value = "user.register.request", description = "Time taken to register a user.")
     public ResponseEntity<?> registerUser(@RequestBody User user) {
         try {
             User createdUser = userService.createUser(user);
+            logger.info("successfully registered user : {}", user);
             return ResponseEntity.ok(createdUser);
         } catch (Exception e) {
+            logger.error("Exception registering a user:  ", e);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body("Error registering user: " + e.getMessage());
         }
