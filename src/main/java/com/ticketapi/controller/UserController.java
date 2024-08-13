c67589d0ac88370621433adb4fa50fa47eaf5380
Girmay-1 <girmaykibreab93@gmail.com>
2024-08-12 17:33:09 -0700

    added ELK stack, Prometheus and Grafana for metrics and monitoring

--- src/main/java/com/ticketapi/controller/UserController.java
+++ src/main/java/com/ticketapi/controller/UserController.java
@@ -2,9 +2,12 @@ package com.ticketapi.controller;
 
 import com.ticketapi.model.User;
 import com.ticketapi.service.CustomUserDetailsService;
+import io.micrometer.core.annotation.Timed;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -18,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Users", description = "User operations")
 @SecurityRequirement(name = "jwt_auth")
 public class UserController {
-
+    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
     private final CustomUserDetailsService userDetailsService;
 
     public UserController(CustomUserDetailsService userDetailsService) {
@@ -27,20 +30,25 @@ public class UserController {
 
     @GetMapping("/profile")
     @Operation(summary = "Get the profile of the authenticated user")
+    @Timed(value = "user.profile.request", description = "Time taken to return user profile")
     public ResponseEntity<?> getUserProfile(Authentication authentication) {
         if (authentication == null) {
+            logger.warn("unauthorized access attempt to user prifle");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body("User not authenticated");
         }
 
         try {
             String username = authentication.getName();
+            logger.info("Fetching user profile for user: {}", username);
             User user = (User) userDetailsService.loadUserByUsername(username);
             return ResponseEntity.ok(user);
         } catch (UsernameNotFoundException e) {
+            logger.error("User not found: {}", authentication.getName(), e);
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("User not found");
         } catch (Exception e) {
+            logger.error("Error retrieving user profile for user: {}", authentication.getName(), e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error retrieving user profile");
         }
