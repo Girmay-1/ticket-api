0c3fb79e12d8c6212520d382e44e6c74a601a39e
Girmay-1 <girmaykibreab93@gmail.com>
2024-08-14 11:29:03 -0700

    1. Integerate Liquibase and update Docker configuration for database. \ 2. Add Liquibase for database schema versioning. \ 3. Create initial SQL changelog for users, events and tickets tables. \ 4. Update docker compose to use DB_URL and ensure database readiness \ 5. Adjust application.properties for flexible database connection \ 6. Update GitHub Actions workflow for Postgres service with healthcheck in CI/CD pipeline \ 7. Configure Database credentials using GitHub secrets. \ 8. Setup environment varialbes for database connection

---
+++ src/main/resources/db/changelog/db.changelog-master.sql
@@ -0,0 +1,37 @@
+--liquibase formatted sql
+
+--changeset Girmay:1
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       username VARCHAR(255) NOT NULL UNIQUE,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       password_hash VARCHAR(255) NOT NULL,
+                       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                       is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+--rollback DROP TABLE users;
+
+--changeset Girmay:2
+CREATE TABLE events (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(255) NOT NULL,
+                        description TEXT,
+                        date_time TIMESTAMP NOT NULL,
+                        venue VARCHAR(255) NOT NULL,
+                        total_tickets INT NOT NULL,
+                        available_tickets INT NOT NULL
+);
+--rollback DROP TABLE events;
+
+--changeset Girmay:3
+CREATE TABLE tickets (
+                         id SERIAL PRIMARY KEY,
+                         event_id INT NOT NULL,
+                         user_id INT NOT NULL,
+                         ticket_type VARCHAR(255),
+                         price DECIMAL(10, 2) NOT NULL,
+                         FOREIGN KEY (event_id) REFERENCES events(id),
+                         FOREIGN KEY (user_id) REFERENCES users(id)
+);
+--rollback DROP TABLE tickets;
